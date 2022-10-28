5b25d00527d0e52acde5df7aec503eea50323c48
Joe D <ony.dickson@gmail.com>
2022-10-27 23:29:39 -0500

    fixing issues 2

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,12 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name = "Joe";
+		String name = "vamsi";
 
 		System.out.println(name);
-
-		Integer age = "20";
-		System.out.println(age);
 	}
 
 }
