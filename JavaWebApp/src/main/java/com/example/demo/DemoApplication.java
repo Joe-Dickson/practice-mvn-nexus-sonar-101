c379c42e063ac389fb68cee7096efaea2aaa847f
Joe D <ony.dickson@gmail.com>
2022-10-27 22:16:38 -0500

    adding the sample jave project to my repo

---
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+		string name = "Joe"
+
+		System.out.println(name);
+
+		Integer age = "20";
+		System.out.println(age);
+	}
+
+}
